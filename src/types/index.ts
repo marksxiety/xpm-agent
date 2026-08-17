332ad90b689609d750257a3d9f8a14ef6209c1f3
Mark Ciril <markciril.jamon@gmail.com>
2026-08-17 10:18:10 +0800

    FEAT: add ProcessSummary and ApiResponse interfaces to type definitions

---
+++ src/types/index.ts
@@ -0,0 +1,25 @@
+
+export interface ProcessSummary {
+    pid: number;
+    pm_id: number;
+    name: string;
+    namespace: string;
+    status: string;
+    uptime: number | undefined;
+    restarts: number;
+    unstable_restarts: number;
+    exec_mode: string;
+    instances: number | undefined;
+    interpreter: string;
+    cpu: number;
+    memory: number;
+    cwd: string | undefined;
+    watch: boolean;
+    autorestart: boolean | undefined;
+}
+
+export interface ApiResponse<T = unknown> {
+    success: boolean;
+    message: string;
+    info: T
+}
