80b3f70c35e6741b95f7271c8abdff5ccc6b4c04
Mark Ciril <markciril.jamon@gmail.com>
2026-08-23 14:41:21 +0800

    REF: simplify getCurrentTimeStamp to Date.now, drop timezone logic

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -1,23 +1,3 @@
-import { config } from "../config";
-
-export function getCurrentTimeStamp(timezone: string = config.SERVER_TIMEZONE): number {
-  const parts = new Intl.DateTimeFormat('en-US', {
-    timeZone: timezone,
-    year: 'numeric', month: '2-digit', day: '2-digit',
-    hour: '2-digit', minute: '2-digit', second: '2-digit',
-    fractionalSecondDigits: 3,
-    hour12: false,
-  }).formatToParts(new Date());
-
-  const get = (type: string) => parts.find(p => p.type === type)!.value;
-
-  return Date.UTC(
-    Number(get('year')),
-    Number(get('month')) - 1,
-    Number(get('day')),
-    Number(get('hour') === '24' ? '0' : get('hour')),
-    Number(get('minute')),
-    Number(get('second')),
-    Number(get('fractionalSecond') ?? 0)
-  );
+export function getCurrentTimeStamp(): number {
+  return Date.now();
 }
