a0b0cffd7a8e38df28393c046310638e20acfbe8
Mark Ciril <markciril.jamon@gmail.com>
2026-08-31 10:13:02 +0800

    CHORE: point pm2 script to dist/index.js and add NODE_ENV env variants

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,22 @@
 module.exports = {
-    apps: [{
-        name: "xpm-agent",
-        namespace: 'XPM',
-        cwd: __dirname,
-        script: "bun",
-        args: "src/index.ts",
-        interpreter: "none",
-        exec_mode: "fork",
-        instances: 1,
-        autorestart: true,
-        max_restarts: 10,
-        time: true,
-    }],
+  apps: [
+    {
+      name: "xpm-agent",
+      namespace: "XPM",
+      cwd: __dirname,
+      script: "./dist/index.js",
+      interpreter: "bun",
+      exec_mode: "fork",
+      instances: 1,
+      autorestart: true,
+      max_restarts: 10,
+      time: true,
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
 };
